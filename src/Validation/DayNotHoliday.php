b9810a97c44b6414516be58e206f7443d181b80b
Veneridze <viacheslav.kopaev@yandex.ru>
2024-11-02 22:59:40 +0300

    Update

---
+++ src/Validation/DayNotHoliday.php
@@ -0,0 +1,28 @@
+<?php
+namespace Veneridze\LaravelMarker\Validation;
+
+use Closure;
+use Exception;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Carbon;
+use Veneridze\LaravelHoliday\Models\Holiday;
+
+class DayNotHoliday implements ValidationRule {
+    /**
+     * Validation rule
+     * @param string $attribute
+     * @param mixed $value
+     * @param \Closure $fail
+     * @return void
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void {
+        try {
+            $date = Carbon::parse($value);
+            if(Holiday::isHoliday($date)) {
+                $fail('День является выходным');
+            }
+        } catch (Exception $e) {
+            $fail($e->getMessage());
+        }
+    }
+}
